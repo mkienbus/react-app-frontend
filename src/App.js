8ec15b5a65aef6d6bea61548fa6100f23541cae0
Michael Kienbsuch <mkienbus@gmail.com>
2021-09-16 19:39:30 -0600

    setup FirearmContainer, FirearmForm, and App components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-//git init test
+import React from "react"
+import FirearmContainer from './FirearmContainer';
+import FirearmForm from './FirearmForm';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <FirearmContainer />
+      <FirearmForm /> 
     </div>
   );
+  //App.js render a firearm container component and new firearm form component (for POSTing)
+      //firearm container component would render firearm cards
+  //nav component? about component?
 }
 
 export default App;
---
+++ src/FirearmForm.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function FirearmForm(){
+    return
+    <div>
+        <form>
+            User fills in object data
+        </form>
+    </div>
+}
+
+export default FirearmForm
