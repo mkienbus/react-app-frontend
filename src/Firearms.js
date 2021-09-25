5009a6918aa6d8fc697348e098f1e601eabb6011
Michael Kienbsuch <mkienbus@gmail.com>
2021-09-24 18:30:22 -0600

    implementation of firearms.map gives error of '.map not a function'; might be the 'firearms' data passed as prop from App component is not proper data type

--- src/Firearms.js
+++ src/Firearms.js
@@ -3,14 +3,14 @@ import FirearmCard from "./FirearmCard"
 // this is a "container" component because it renders other components
 
 function Firearms(firearms){
-    console.log(firearms)
 
-    const allFirearms = firearms.map(firearm => <FirearmCard /> )
+    const allFirearms = firearms.map(firearm => <FirearmCard firearm = {firearm}/> )
 
     return(
-    <div>
-        <h1>My Firearms</h1>
-    </div>
+        <div>
+            <h1>My Firearms</h1>
+            {allFirearms}
+        </div>
     )
     //render firearm cards here, which means it needs access to the data
 }
