6c700ed7c37831a6517ada65971cbe723a7892e6
Michael Kienbsuch <mkienbus@gmail.com>
2021-09-28 10:03:15 -0600

    added NavLinks to Nav component, stylized with 10px margins for readability in DOM

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,13 @@
 import React from "react"
+import {NavLink} from "react-router-dom"
 
 function Nav(){
     return(
         <div>
-            <h2>Nav </h2>
+            <h2>Navigation Bar</h2>
+            <NavLink style = {{marginRight: "10px"}} to = "/">Home</NavLink>
+            <NavLink style = {{marginRight: "10px"}} to = "/firearms">Firearms List</NavLink>
+            <NavLink style = {{marginRight: "10px"}} to = "/firearms/new">Add a Firearm to List</NavLink>
         </div>
     )
 }
