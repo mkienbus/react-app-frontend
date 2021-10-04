1d3870d848a155a8b5139e151396b3f96cd5330d
Michael Kienbsuch <mkienbus@gmail.com>
2021-10-04 13:19:10 -0600

    full function

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const [firearms, setFirearms] = useState([]);
   //const [firearm, setFirearm] = useState("");
-  let firearm = {}
+  //let firearm = {}
   
   useEffect(()=>{
     fetch('http://localhost:3000/firearms')
@@ -27,7 +27,6 @@ function App() {
   }
 
   function handleClick(detailFirearm) {
-    console.log(detailFirearm.name)
   }
 
   return (
@@ -58,11 +57,3 @@ export default App;
 //App.js render a firearm container component and new firearm form component (for POSTing)
       //firearmscomponent would render firearm cards
   //nav component
-
-  //which component should handle fetch to get data
-    //app component
-  //which components should have state? 
-    //-form -toys "container" component
-    // app component
-
-    //have state for child components live in app component and have app handle fetching data
