02703e2796d3eba517b7a088f5db028a90a319d7
Michael Kienbsuch <mkienbus@gmail.com>
2021-10-13 14:38:32 -0600

    removed native JS, used react controlled input instead - passed

--- src/ClickMe.js
+++ src/ClickMe.js
@@ -6,33 +6,38 @@ function ClickMe(){
 
     const [count, setCount] = useState("");
     const [displayedCount, setDisplayedCount] = useState(0);
+    const [value, setValue] = useState('');
 
     function handleChange(e){
         let word = (e.target.value)
         console.log(word)
         setCount(word.length)
         console.log(word.length)
+        setValue(word)
     }
 
     function handleClickSubmit(e){
         e.preventDefault();
         addCounts(e);
         setCount("")
-        let textReset = document.getElementById("formText")
-        textReset.value = ""
     }
 
     function addCounts(e){
         e.preventDefault();
         setDisplayedCount(count + displayedCount)
     }
+
+    function resetValue(e){
+        e.preventDefault();
+        setValue("")
+    }
     
     return(
         <div>
-            <form onSubmit = {(e) => {handleClickSubmit(e); addCounts(e)}}>
+            <form onSubmit = {(e) => {handleClickSubmit(e); addCounts(e); resetValue(e)}}>
                 <label>
                     Type:
-                        <input id = "formText" type = "text" onChange = {handleChange}/>
+                        <input id = "formText" type = "text" value = {value} onChange = {handleChange}/>
                 </label>
                     <input type = "submit" value = "Click Me"/>
                 <h3>{displayedCount}</h3>
